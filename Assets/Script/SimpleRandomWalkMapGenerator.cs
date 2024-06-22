4fb70de7ecd3828eb041bb34c8c0ac3c484e1236
Muto <Mahmut.Cinar93@outlook.de>
2024-06-22 14:28:49 +0200

    Generating Floorpositions with SimpleRandomWalk

--- Assets/Script/SimpleRandomWalkMapGenerator.cs
+++ Assets/Script/SimpleRandomWalkMapGenerator.cs
@@ -1,13 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SimpleRandomWalkMapGenerator : MonoBehaviour
 {
     [SerializeField]
     protected Vector2Int startingPoint = Vector2Int.zero;
-
+    [SerializeField]
     private int iterations = 10;
+    [SerializeField]
     public int pathLength = 10;
-    public bool randomStartPerIteration = true;
+    [SerializeField]
+    public bool randomStartEachIteration = true;
+
+    public void startProceduralGeneration()
+    {
+        HashSet<Vector2Int> floorPoints = startRandomWalk();
+        foreach (var point in floorPoints)
+        {
+            Debug.Log(point);
+        }
+    }
+
+    protected HashSet<Vector2Int> startRandomWalk()
+    {
+        var currentPoint = startingPoint;
+        HashSet<Vector2Int> floorpoints = new HashSet<Vector2Int>();
+        for (int i = 0; i < iterations; i++) 
+        {
+            var path = ProceduralGenerationAlgorithms.SimpleRandomWalk(currentPoint, pathLength);
+            floorpoints.UnionWith(path); // Union to Add without Duplicates
+            if (randomStartEachIteration)
+            {
+                currentPoint = floorpoints.ElementAt(Random.Range(0, floorpoints.Count));
+            }
+        }
+        return floorpoints;
+    }
 }
