f63776d6a9509955824afa96dacae5af8cd9d061
zDanaa <danielhoeg.03@gmail.com>
2024-06-26 21:28:58 +0200

    Implemented timer and added game manager for handling global events

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float gameTime = 10 * 60;
+    public int miniBossSpawnTime = 5 * 60;
+    public Text timerText;
+
+    void Start()
+    {
+        StartCoroutine(GameTimer());
+    }
+
+    IEnumerator GameTimer()
+    {
+        float remainingTime = gameTime;
+        while (remainingTime > 0)
+        {
+            if (Mathf.Approximately(remainingTime, miniBossSpawnTime)) 
+            {
+                SpawnMiniBoss();
+            }
+
+            remainingTime--;
+
+            string minutes = Mathf.FloorToInt(remainingTime / 60F).ToString();
+            string seconds = Mathf.FloorToInt(remainingTime % 60).ToString();
+            minutes = minutes.Length == 1 ? minutes = "0" + minutes : minutes;
+            seconds = seconds.Length == 1 ? seconds = "0" + seconds : seconds;
+            timerText.text = minutes + ":" + seconds;
+
+            yield return new WaitForSeconds(1f);
+        }
+        PlayerWins();
+    }
+
+    void SpawnMiniBoss()
+    {
+        print("Spawning mini boss");
+    }
+
+    void PlayerWins()
+    {
+        Debug.Log("Player has won the game!");
+    }
+}
