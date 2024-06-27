536d7dc2441dc5ceb7c8b0de2ca5807dfbabe00f
Muto <Mahmut.Cinar93@outlook.de>
2024-06-28 00:58:02 +0200

    First Approach on BinarySpacePartion

--- Assets/Script/ProceduralGenerationAlgorithms.cs
+++ Assets/Script/ProceduralGenerationAlgorithms.cs
@@ -34,6 +34,62 @@ public static class ProceduralGenerationAlgorithms
         }
         return corridor;
     }
+
+    public static List<BoundsInt> binarySpacePartioning(BoundsInt spaceToSplit, int minWidth, int minHeight)
+    {
+        Queue<BoundsInt> roomsQueue = new Queue<BoundsInt>();
+        List<BoundsInt> roomsList = new List<BoundsInt>();
+        roomsQueue.Enqueue(spaceToSplit);
+        while(roomsQueue.Count > 0)
+        {
+            var room = roomsQueue.Dequeue();
+            if(room.size.y<minHeight && room.size.x >= minWidth)
+            {
+                if(Random.value < 0.5f)
+                {
+                    if (room.size.y >= minHeight * 2)
+                    {
+                        SplitHorizontally(minWidth, minHeight, roomsQueue, room);
+                    }
+                    else if (room.size.x >= minWidth * 2)
+                    {
+                        SplitVertically(minWidth, minHeight, roomsQueue, room);
+                    }
+                    else if (room.size.x >= minWidth && room.size.y >= minHeight)
+                    { 
+                        roomsList.Add(room);
+                    }                }
+                else
+                {
+
+                    if (room.size.x >= minWidth * 2)
+                    {
+                        SplitVertically(minWidth, minHeight, roomsQueue, room);
+                    }
+
+                    else if (room.size.y >= minHeight * 2)
+                    {
+                        SplitHorizontally(minWidth, minHeight, roomsQueue, room);
+                    }
+                    else if (room.size.x >= minWidth && room.size.y >= minHeight)
+                    {
+                        roomsList.Add(room);
+                    }
+                }
+            }
+        }
+        return roomsList;
+    }
+
+    private static void SplitVertically(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private static void SplitHorizontally(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    {
+        throw new System.NotImplementedException();
+    }
 }
 
 public static class Direction2D
