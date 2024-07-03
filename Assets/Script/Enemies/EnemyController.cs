6313f1775a86e369225af6bd8be5552f3c094d42
zDanaa <danielhoeg.03@gmail.com>
2024-07-03 21:33:22 +0200

    Included player controller script in enemy super class

--- Assets/Script/Enemies/EnemyController.cs
+++ Assets/Script/Enemies/EnemyController.cs
@@ -10,6 +10,7 @@ public class EnemyController : MonoBehaviour
     public HealthBarScript healthBar;
     public Transform target;
     public SpriteRenderer spriteRenderer;
+    public PlayerController player;
     public Animator animator;
     public float speed;
     public float attackTimer;
