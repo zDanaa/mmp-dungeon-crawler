3a15def3571b4e32e47b1f557f90100aab90ff5b
zDanaa <danielhoeg.03@gmail.com>
2024-07-04 22:18:57 +0200

    Implemented shooting enemy animation

--- Assets/Script/Enemies/ShootingEnemyController.cs
+++ Assets/Script/Enemies/ShootingEnemyController.cs
@@ -24,31 +24,39 @@ public class ShootingEnemyController : EnemyController
     }
     void Update()
     {
-        if (target == null)
-        {
-            return;
-        }
+        if (target == null) return;
+
+        setSpriteFlip(target.position - transform.position, -1);
 
         float distanceToTarget = Vector2.Distance(transform.position, target.position);
+        if (distanceToTarget > aggroRange) 
+        {
+            animator.SetBool("Idle", true);
+            animator.SetBool("Walk", false);
+        }
+        else {
+            animator.SetBool("Idle", false);
+            animator.SetBool("Walk", true);
+            if (distanceToTarget > safeZone)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+            else if (distanceToTarget < dangerZone)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
+            }
+            else if (distanceToTarget < safeZone && distanceToTarget > dangerZone)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, (speed / 2) * Time.deltaTime);
+            }
 
-        if (distanceToTarget > safeZone)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        }
-        else if (distanceToTarget < dangerZone)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
-        }
-        else if (distanceToTarget < safeZone && distanceToTarget > dangerZone)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, (speed / 2) * Time.deltaTime);
-        }
-
-        attackTimer += Time.deltaTime;
-        if (attackTimer > attackCooldown)
-        {
-            attackTimer = 0;
-            Shoot();
+            attackTimer += Time.deltaTime;
+            if (attackTimer > attackCooldown)
+            {
+                attackTimer = 0;
+                animator.SetTrigger("Attack");
+                Shoot();
+            }
         }
     }
 
