7108aac8403375f3b85f8295d693f6440a391033
Muto <Mahmut.Cinar93@outlook.de>
2024-07-07 22:43:20 +0200

    Implement Item with placement property

---
+++ Assets/Script/DungeonGeneration/RoomSystem/Items/Item.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private BoxCollider2D itemCollider;
+
+    [SerializeField]
+    int health = 3;
+    [SerializeField]
+    bool nonDestructible;
+    public void Initialize(ItemData itemData)
+    {
+        //set sprite
+        spriteRenderer.sprite = itemData.sprite;
+        //set sprite offset
+        spriteRenderer.transform.localPosition = new Vector2(0.5f * itemData.size.x, 0.5f * itemData.size.y);
+        itemCollider.size = itemData.size;
+        itemCollider.offset = spriteRenderer.transform.localPosition;
+
+        if (itemData.nonDestructible)
+            nonDestructible = true;
+
+        this.health = itemData.health;
+
+    }
+}
