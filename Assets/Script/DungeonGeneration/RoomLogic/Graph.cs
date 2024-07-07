3fc7f485cd8aae332750f9acc7321e2864252654
Muto <Mahmut.Cinar93@outlook.de>
2024-07-07 21:48:23 +0200

    Create Graph for Room Logic

---
+++ Assets/Script/DungeonGeneration/RoomLogic/Graph.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    private static List<Vector2Int> neighbours4directions = new List<Vector2Int>
+    {
+        new Vector2Int(0,1), // UP
+        new Vector2Int(1,0), // RIGHT
+        new Vector2Int(0,-1), // DOWN
+        new Vector2Int(-1,0) // LEFT
+    };
+
+    private static List<Vector2Int> neighbours8directions = new List<Vector2Int>
+    {
+        new Vector2Int(0,1), // UP
+        new Vector2Int(1,0), // RIGHT
+        new Vector2Int(0,-1), // DOWN
+        new Vector2Int(-1,0), // LEFT
+        new Vector2Int(1,1), // Diagonal
+        new Vector2Int(1,-1), // Diagonal
+        new Vector2Int(-1,1), // Diagonal
+        new Vector2Int(-1,-1) // Diagonal
+    };
+
+    List<Vector2Int> graph;
+
+    public Graph(IEnumerable<Vector2Int> vertices)
+    {
+        graph = new List<Vector2Int>(vertices);
+
+    }
+
+    public List<Vector2Int> GetNeighboursBasicDirections(Vector2Int startPoint)
+    {
+        return GetNeighbours(startPoint, neighbours4directions);
+    }
+    public List<Vector2Int> GetNeighboursAllDirections(Vector2Int startPoint)
+    {
+        return GetNeighbours(startPoint, neighbours8directions);
+    }
+
+    private List<Vector2Int> GetNeighbours(Vector2Int startPoint, List<Vector2Int> neighboursOffsetList)
+    {
+        List<Vector2Int> neighbours = new List<Vector2Int>();
+        foreach(var neighbourDirection in neighboursOffsetList)
+        {
+            Vector2Int potentialNeighbour = startPoint + neighbourDirection;
+            if (graph.Contains(potentialNeighbour))
+            {
+                neighbours.Add(potentialNeighbour);
+            }
+        }
+        return neighbours;
+    }
+}
