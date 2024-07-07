e1ab28d7192a19460565141fd5135a5fd0fd95e3
Muto <Mahmut.Cinar93@outlook.de>
2024-07-07 22:07:29 +0200

    Implement ItemPlacementHelper

---
+++ Assets/Script/DungeonGeneration/RoomSystem/Items/ItemPlacementHelper.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ItemPlacementHelper
+{
+    Dictionary<PlacementType, HashSet<Vector2Int>>
+        tileByType = new Dictionary<PlacementType, HashSet<Vector2Int>>();
+
+    HashSet<Vector2Int> roomFloorWithoutCorridor;
+
+    public ItemPlacementHelper(HashSet<Vector2Int> roomFloor, HashSet<Vector2Int> roomFloorWithoutCorridor)
+    {
+        Graph graph = new Graph(roomFloor);
+        this.roomFloorWithoutCorridor = roomFloorWithoutCorridor;
+
+        foreach (var point in roomFloorWithoutCorridor)
+        {
+            int neighboursCountAllDir = graph.GetNeighboursAllDirections(point).Count;
+            PlacementType type = neighboursCountAllDir < 8 ? PlacementType.NearWall : PlacementType.OpenSpace;
+
+            if (!tileByType.ContainsKey(type))
+            {
+                tileByType[type] = new HashSet<Vector2Int>();
+            }
+
+            if (type == PlacementType.NearWall && graph.GetNeighboursBasicDirections(point).Count == 4)
+            {
+                continue;
+            }
+            tileByType[type].Add(point);
+        }
+    }
+
+    public Vector2? GetItemPlacementPosition(PlacementType placementType, int maxIterations, Vector2Int size, bool addOffset)
+    {
+        int itemArea = size.x * size.y;
+        if (tileByType[placementType].Count < itemArea)
+            return null;
+
+        int iteration = 0;
+        while (iteration < maxIterations)
+        {
+            iteration++;
+            int i = UnityEngine.Random.Range(0, tileByType[placementType].Count);
+            Vector2Int point = tileByType[placementType].ElementAt(i);
+
+
+            if (itemArea > 1)
+            {
+                var (result, placementPositions) = PlaceBigItem(point, size, addOffset);
+
+                if(result == false)
+                {
+                    continue;
+                }
+
+                tileByType[placementType].ExceptWith(placementPositions);
+                tileByType[PlacementType.NearWall].ExceptWith(placementPositions);
+            }
+            else
+            {
+                tileByType[placementType].Remove(point);
+            }
+            return point;
+        }
+        return null;
+
+    }
+
+    private (bool, List<Vector2Int>) PlaceBigItem(Vector2Int initialPoint, Vector2Int size, bool addOffset)
+    {
+        List<Vector2Int> points = new List<Vector2Int>() { initialPoint };
+        int maxX = addOffset ? size.x + 1 : size.x;
+        int maxY = addOffset ? size.y + 1 : size.y;
+        int minX = addOffset ? -1 : 0;
+        int minY = addOffset ? -1 : 0;
+
+        for (int row = minX; row <= maxX; row++)
+        {
+            for (int col = minY; col <= maxY; col++)
+            {
+                if (col == 0 && row == 0)
+                {
+                    continue;
+                }
+                Vector2Int newPointsToCheck = new Vector2Int(initialPoint.x + row, initialPoint.y + col);
+                if (!roomFloorWithoutCorridor.Contains(newPointsToCheck))
+                {
+                    return (false, points);
+                }
+                points.Add(newPointsToCheck);
+            }
+        }
+        return (true, points);
+    }
+}
+    public enum PlacementType
+{
+    OpenSpace,
+    NearWall
+}
+
