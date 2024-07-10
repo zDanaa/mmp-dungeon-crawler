179861d42d3dc8cefedf1bf616f36306ebd21abc
Muto <Mahmut.Cinar93@outlook.de>
2024-07-10 18:38:30 +0200

    Make the camera focus on player after spawning and add PlayerComponent to playerroom

--- Assets/Script/Player/CameraController.cs
+++ Assets/Script/Player/CameraController.cs
@@ -22,5 +22,32 @@ public class CameraController : MonoBehaviour
             Camera.main.transform.position.z 
         );
         }
+        else
+        {
+            GameObject roomsContent = GameObject.Find("RoomsContent");
+            if (roomsContent != null)
+            {
+                // Finde alle GameObjects mit dem Tag "Player"
+                GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+                foreach (GameObject potentialPlayer in players)
+                {
+                    if (potentialPlayer.transform.IsChildOf(roomsContent.transform))
+                    {
+                        player = potentialPlayer;
+                        Debug.Log("Player gefunden: " + player.name);
+                        break;
+                    }
+                }
+
+                if (player == null)
+                {
+                    Debug.LogError("Player GameObject nicht gefunden!");
+                }
+            }
+            else
+            {
+                Debug.LogError("RoomsContent GameObject nicht gefunden!");
+            }
+        }
     }
 }
